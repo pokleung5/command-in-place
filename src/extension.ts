6fb74f19976d87b9d3ca8258e3bddb68589996fc
eric.leung <you@example.com>
2020-12-31 23:34:23 +0800

    Add history selection

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,7 @@ import { Transform } from 'stream';
 const EXTENSION_NAME = "command-in-place"
 const fn_symbol = "%fn%";
 const delim = getConfig<string>("delimForTime");
+var command_history: vscode.QuickPickItem[] = [];
 
 function getConfig<T>(configPath: string): T {
 	return vscode.workspace.getConfiguration().get(EXTENSION_NAME + "." + configPath) as T;
@@ -16,67 +17,98 @@ export function activate(context: vscode.ExtensionContext) {
 
 	var l_command = fn_symbol;
 
-	let disposable = vscode.commands.registerCommand(EXTENSION_NAME + '.runCommand',
+	context.subscriptions.push(vscode.commands.registerCommand(EXTENSION_NAME + '.showHistory',
+		async () => {
+			if (command_history.length > 0) {
+				vscode.window.showQuickPick(command_history, {
+					matchOnDescription: true,
+				}).then(selection => {
+					if (!selection) {
+						return;
+					}
+					handleInCmd(selection.label);
+				});
+			} else { 
+				vscode.window.showErrorMessage(
+					"No history"
+				);
+			}
+		})
+	);
+
+	context.subscriptions.push(vscode.commands.registerCommand(EXTENSION_NAME + '.runCommand',
 		async () => {
 			const command = await vscode.window.showInputBox({
 				placeHolder: 'Enter a command',
 				value: l_command
 			});
 
-			const editor = vscode.window.activeTextEditor;
-
-			if (command == undefined || editor == undefined) {
+			if (command === undefined) {
 				return 1;
 			}
 
-			let n_command = command;
 			l_command = command;
-
-			let term = getConfig<string>("term");
-
-			if (command.includes(fn_symbol)) {
-				let fn_path = editor.document.uri.fsPath;
-
-				if (fn_path.includes(":")) {
-					if (term === 'cmd') {
-						n_command = command.replace(fn_symbol, fn_path);
-					} else {
-						fn_path = "\\" + fn_path.replace(":", "");
-						fn_path = fn_path.replace(/\\/g, "/");
-
-						n_command = command.replace(fn_symbol, fn_path);
-					}
-				}
-				else {
-					vscode.window.showErrorMessage(
-						"Uri not correct: " + editor.document.uri.toString()
-					);
-					return 0;
-				}
-			}
-
-			await Promise.all(editor.selections.map(selection =>
-				exec(term, n_command, editor.document.getText(selection))
-			)).then(resolves => {
-				editor.edit(editBuilder => {
-					editor.selections.forEach((selection, index) => {
-						editBuilder.replace(selection, resolves[index]);
-					});
-				});
-			}).catch(rejects => {
-				outWin.clear();
-				outWin.append(rejects);
-
-				vscode.window.showErrorMessage("Please check output window !");
-				outWin.show();
-			});
-
-		});
-
-	context.subscriptions.push(disposable);
+			handleInCmd(command.trimLeft().trimRight());
+		})
+	);
 }
 
 export function deactivate() { }
+
+async function handleInCmd(command: string) {
+
+	const editor = vscode.window.activeTextEditor;
+	if (!editor) {
+		return;
+	}
+
+	let n_command = command;
+	let term = getConfig<string>("term");
+
+	if (command.includes(fn_symbol)) {
+		let fn_path = "\"" + editor.document.uri.fsPath + "\"";
+
+		if (fn_path.includes(":")) {
+			if (term === 'cmd') {
+				n_command = command.replace(fn_symbol, fn_path);
+			} else {
+				fn_path = "\\" + fn_path.replace(":", "");
+				fn_path = fn_path.replace(/\\/g, "/");
+				n_command = command.replace(fn_symbol, fn_path);
+			}
+		}
+		else {
+			vscode.window.showErrorMessage(
+				"Uri not correct: " + editor.document.uri.toString()
+			);
+			return;
+		}
+	}
+
+	command_history = command_history.filter(x => {
+		return x.label !== command;
+	});
+
+	await Promise.all(editor.selections.map(selection =>
+		exec(term, n_command, editor.document.getText(selection))
+	)).then(resolves => {
+		editor.edit(editBuilder => {
+			editor.selections.forEach((selection, index) => {
+				editBuilder.replace(selection, resolves[index]);
+			});
+		});
+		if (command_history.length >= getConfig<number>("maxhistory")) {
+			command_history.pop();
+		}
+		command_history.unshift({
+			'label': command
+		});
+	}).catch(rejects => {
+		outWin.clear();
+		outWin.append(rejects);
+		outWin.show();
+	});
+}
 
 function exec(term: string, command: string, input: string): Promise<string> {
 	let spawn = require('child_process').spawn;
